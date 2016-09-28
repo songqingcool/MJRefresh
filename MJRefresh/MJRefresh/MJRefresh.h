b317ad438e01ee8294fd69416c1d4700e920d9ca
songqinggong <songqinggong@syswin.com>
2016-09-28 14:47:45 +0800

    上传源代码

---
+++ MJRefresh/MJRefresh/MJRefresh.h
@@ -0,0 +1,35 @@
+//
+//  MJRefresh.h
+//  MJRefresh
+//
+//  Created by 宋庆功 on 2016/9/28.
+//  Copyright © 2016年 思源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJRefresh.
+FOUNDATION_EXPORT double MJRefreshVersionNumber;
+
+//! Project version string for MJRefresh.
+FOUNDATION_EXPORT const unsigned char MJRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJRefresh/PublicHeader.h>
+
+
+//  代码地址: https://github.com/CoderMJLee/MJRefresh
+//  代码地址: http://code4app.com/ios/%E5%BF%AB%E9%80%9F%E9%9B%86%E6%88%90%E4%B8%8B%E6%8B%89%E4%B8%8A%E6%8B%89%E5%88%B7%E6%96%B0/52326ce26803fabc46000000
+
+
+#import <MJRefresh/UIScrollView+MJRefresh.h>
+#import <MJRefresh/UIScrollView+MJExtension.h>
+#import <MJRefresh/UIView+MJExtension.h>
+
+#import <MJRefresh/MJRefreshNormalHeader.h>
+#import <MJRefresh/MJRefreshGifHeader.h>
+
+#import <MJRefresh/MJRefreshBackNormalFooter.h>
+#import <MJRefresh/MJRefreshBackGifFooter.h>
+#import <MJRefresh/MJRefreshAutoNormalFooter.h>
+#import <MJRefresh/MJRefreshAutoGifFooter.h>
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MJRefresh
-ww
+MJRefresh的简单封装framework静态库
+
+MJRefresh源代码下载日期 2016年09月28日14:46:28
+
+clone项目之后直接运行MJRefreshAgg即可得到编译好的MJRefresh.framework(真机和模拟器)
+
+1、iOS8以上
+
+2、支持bitcode
+
